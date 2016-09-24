92bbaa2424e487dfbf2101496b7aa63ec6c23f2c
JoeSz <ujbeszel@gmail.com>
2016-09-24 13:13:42 +0200

    Optimalization

--- jquery.sticky-anything.js
+++ jquery.sticky-anything.js
@@ -16,7 +16,7 @@
         // Insert an empty div, for placeholder and measuring purposes
         $( '<div></div>' ).addClass( $( this ).attr( 'class' ) ).insertAfter( this );
 
-        $( window ).bind( 'load scroll resize', function() {
+        var runOnScreensize = function() {
             // Calculating actual viewport width
             var e = window, a = 'inner';
             if ( ! ( 'innerWidth' in window ) ) {
@@ -25,31 +25,31 @@
             }
             var viewport = e[ a + 'Width' ];
 
-            if ( ( viewport >= settings.minscreenwidth ) && ( viewport <= settings.maxscreenwidth ) ) {
-                stickIt( settings.top, settings.zindex, settings.fixedClass, thisObject );
+            return ( ( viewport >= settings.minscreenwidth ) && ( viewport <= settings.maxscreenwidth ) );
+        };
+
+        var runThis = function( callingEvent ) {
+            if ( runOnScreensize ) stickIt( settings.top, settings.zindex, settings.fixedClass, thisObject, callingEvent );
+        }
+
+        $(window).on({
+             resize:function(){
+                runThis('resize');
+             },
+             scroll:function(){
+                runThis('scroll');
             }
         });
 
         return this;
     };
 
-    function calculatePosition( placeholder, zindex, thisObject ) {
-        // Calculate object position based on placeholder position
-        var placeholderRight = ( $( window ).width() - ( placeholder.offset().left + placeholder.outerWidth() ) );
-
-        thisObject.css({
-            'max-width': placeholder.outerWidth() + 'px',
-            left: placeholder.offset().left,
-            right: placeholderRight,
-            'z-index': zindex
-        });
-    }
-
-    function stickIt( stickyTop, zindex, fixedClass, thisObject ) {
+    function stickIt( stickyTop, zindex, fixedClass, thisObject, callingEvent ) {
         var placeholder = thisObject.next();
         var placeholderTop = placeholder.offset().top;
         var selectorHeight = thisObject.height();
         var isFixed = thisObject.css("position") === 'fixed';
+        var fixedInit = false;
 
         // Check WordPress admin bar
         var adminBarheight = ( $( '#wpadminbar' ).length && $( '#wpadminbar' ).css( 'position' ) === 'fixed' ) ? $( '#wpadminbar' ).height() : 0;
@@ -59,16 +59,30 @@
             thisObject.addClass( fixedClass );
             thisObject.css( { 'position': 'fixed', top: stickyTop + 'px' } );
             placeholder.css( {height: selectorHeight} );
-            isFixed = true;
+            fixedInit = true;
         } else if ( ( $( window ).scrollTop() < ( placeholderTop - stickyTop ) ) ) {
             thisObject.removeClass( fixedClass );
             thisObject.removeAttr( 'style' );
             placeholder.css( {height: 0} );
         }
 
-        if ( isFixed ) {
-            calculatePosition( placeholder, zindex, thisObject );
+        if ( fixedInit || isFixed && callingEvent == 'resize' ) {
+            // Calculate object position based on placeholder position
+            var placeholderRight = ( $( window ).width() - ( placeholder.offset().left + placeholder.outerWidth() ) );
+
+            thisObject.css({
+                'max-width': placeholder.outerWidth() + 'px',
+                left: placeholder.offset().left,
+                right: placeholderRight,
+                'z-index': zindex
+            });
         };
     }
 
+    $('.main-navigation').stickThis({
+        fixedClass: 'floating-header',
+        zindex: 3,
+    });
+
 }(jQuery));
+
